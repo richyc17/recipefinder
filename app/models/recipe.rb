d1f9128df7f450359e2d7d6dc533a8c385c982d7
Richard Harrington <richyc17@gmail.com>
2017-02-26 20:45:35 -0500

    second

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,7 +6,7 @@ class Recipe
 include HTTParty
 # include httparty
   
-  key_value = ENV['FOOD2FORK_KEY'] || 'a218dcdde4ac0e9b80ee585cca2707a1'
+  key_value = ENV['FOOD2FORK_KEY'] || 'b3f751ff7bbd8c08df7849dee61cc3ae'
   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
   base_uri "http://#{hostport}/api"
   # default_params fields: "count,recipes", key: key_value
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'rspec'
 require 'capybara'
 require 'capybara/dsl'
 require 'capybara/poltergeist'
+require 'capybara/rails' 
 
 RSpec.configure do |config|
   config.include Capybara::DSL
